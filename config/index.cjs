c6cacc72209ae463b2fd27047c3057992e89a0e6
LiuYueXingXi <114907902+LiuYueXingXi@users.noreply.github.com>
2022-10-03 16:27:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,11 +21,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '勇宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o6Gnm6ORZaNxYdXvXHwrsDssVR90',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'e7xPIfwEWs_OcoXYEAQC7oH7FlnptZo1ouh8lg1myyQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '04-13',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
