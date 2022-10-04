e3dab7eb1d4d6aa7d3eb684745351bee61ce7d00
LiuYueXingXi <114907902+LiuYueXingXi@users.noreply.github.com>
2022-10-04 11:08:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,13 +19,33 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '勇宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6Gnm6ORZaNxYdXvXHwrsDssVR90',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Gk7oSqMYpHX4CD7B2Wx61cg0UXYqFkV4JHWDxIAiT6Q',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '04-13',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '勇宝贝', year: '1993', date: '03-22',
+        },
+        {
+          type: '节日', name: '结婚纪念日', year: '2021', date: '10-04',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2013-01-01' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '勇宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o6Gnm6ERiXqR4EwXjJGKr7oQ2mIo',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'Gk7oSqMYpHX4CD7B2Wx61cg0UXYqFkV4JHWDxIAiT6Q',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
