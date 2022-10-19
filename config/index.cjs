b786fe7fcbdb378d351aea28d41240e532715850
LiuYueXingXi <114907902+LiuYueXingXi@users.noreply.github.com>
2022-10-20 00:17:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -104,7 +104,7 @@ const USER_CONFIG = {
       province:'安徽',
       city:'合肥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o6Gnm6ORZaNxYdXvXHwrsDssVR90',
+      id: 'o6Gnm6EkQltXFj2J-SxSZkGMOI7A',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'fuXrhrpUMrFcvW5-HR0y45zawbAYAERNCggxTcTHHl0',
       festivals: [
