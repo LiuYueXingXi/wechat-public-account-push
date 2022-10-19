417225a2c53e537733fe7e37829ef6a872cc104a
LiuYueXingXi <114907902+LiuYueXingXi@users.noreply.github.com>
2022-10-20 00:15:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -81,7 +81,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6Gnm6ORZaNxYdXvXHwrsDssVR90',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '-nZidHmxRMpyebiZrx30qYpbtRggcI-6KmMyRmCQSNE',
+      useTemplateId: 'fuXrhrpUMrFcvW5-HR0y45zawbAYAERNCggxTcTHHl0',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
          {
@@ -106,7 +106,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6Gnm6ORZaNxYdXvXHwrsDssVR90',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '-nZidHmxRMpyebiZrx30qYpbtRggcI-6KmMyRmCQSNE',
+      useTemplateId: 'fuXrhrpUMrFcvW5-HR0y45zawbAYAERNCggxTcTHHl0',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
          {
